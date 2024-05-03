4b5037df0c5c2e8638734e656ecbc8c203c35d62
CDAM777 <168300006+CDAM777@users.noreply.github.com>
2024-05-03 11:23:10 +0000

    gets name and displays message

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -1,14 +1,15 @@
 // Learning C++ 
 // Challenge 01_03
 // Console Interaction, by Eduardo Corpeño 
-
+// This code gets the name of the user and then it prints a message including the name.
 #include <iostream>
 #include <string>
 
 int main(){
     std::string str;
-    std::cin >> str;
-    std::cout << str;
+    std:: cout << "Hello, please type your name:" << std::endl;
+    std::cin >> str;   
+    std::cout << "Hello " << str;
 
     std::cout << std::endl << std::endl;
     return (0);
